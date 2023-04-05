39671a6f981441641a782f995441ceb13033d8d7
Roman Danilevskij <fecc_@mail.ru>
2023-04-05 22:01:59 +0300

    task8-1

--- js/thumbnail.js
+++ js/thumbnail.js
@@ -1,3 +1,5 @@
+import { openModal } from './modal.js';
+
 const template = document.querySelector('#picture').content.querySelector('.picture');
 const container = document.querySelector('.pictures');
 const fragment = document.createDocumentFragment();
@@ -7,16 +9,24 @@ const renderThumbnail = (photo) => {
   thumbnailElement.querySelector('.picture__img').src = photo.url;
   thumbnailElement.querySelector('.picture__comments').textContent = photo.comments.length;
   thumbnailElement.querySelector('.picture__likes').textContent = photo.likes;
+  thumbnailElement.dataset.id = photo.id;
   return thumbnailElement;
 };
- 
+
 const renderThumbnails = (photos) => {
   photos.forEach((photo) => {
     console.log('photo ---', photo);
     fragment.append(renderThumbnail(photo));
   });
   container.append(fragment);
+  container.addEventListener('click', (evt) => {
+    console.log(evt.target);
+    if (evt.target.classList.contains('picture__img')) {
+      const id = evt.target.closest('a').dataset.id * 1;
+      const photo = photos.find((item) => item.id === id);
+      openModal(photo);
+    }
+  });
 };
-
 
 export { renderThumbnails };
